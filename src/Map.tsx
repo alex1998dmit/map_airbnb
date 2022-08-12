4650299a2372a53f7a06e42e25ff238b680927a3
Florian Vallen <f.vallen@geton-institut.de>
2022-08-13 00:19:59 +0200

    Trying to implment filter for apartments
    This is a simple change to outline a problem I am facing
    with another project. I'm trying to implement an airbnb
    like filter but, whenever I filter the apartments and the
    component gets rerendered, the underlying OverlayItems
    don't move in their position. The content changes but the position
    doesn't.

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,62 +1,71 @@
-import { makeStyles } from "@material-ui/core";
-import { useEffect, useRef, useState } from "react";
-import Apartments from "./apartments";
-import MapPoint from "./MapPoint";
-import OverlayContainer from "./OverlayContainer";
+import { makeStyles } from '@material-ui/core';
+import { useEffect, useRef, useState } from 'react';
+import Apartments from './apartments';
+import Apartments2 from './apartments2';
+import MapPoint from './MapPoint';
+import OverlayContainer from './OverlayContainer';
 
 type MapProps = {
-  center: google.maps.LatLngLiteral
-  zoom: number
-}
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+};
 
 const useStyles = makeStyles({
   map: {
-    height: '100vh'
-  }
-})
+    height: '40rem',
+  },
+});
 
 function Map({ center, zoom }: MapProps) {
   const ref = useRef(null);
-  const [map, setMap] = useState<google.maps.Map<Element> | null>(null)
+  const [map, setMap] = useState<google.maps.Map<Element> | null>(null);
+  const [apartments, setApartments] = useState(Apartments);
   const classes = useStyles();
 
   useEffect(() => {
-    if (ref.current) { 
-      let createdMap = new window.google.maps.Map(
-        ref.current,
-        {
-          center,
-          zoom,
-          disableDefaultUI: true,
-          clickableIcons: false
-        }
-      );
-      setMap(createdMap)
+    if (ref.current) {
+      let createdMap = new window.google.maps.Map(ref.current, {
+        center,
+        zoom,
+        disableDefaultUI: true,
+        clickableIcons: false,
+      });
+      setMap(createdMap);
     }
   }, [center, zoom]);
 
-  return <div ref={ref} id="map" className={classes.map}>
-    {Apartments.map((apartment, index) => (
-      <OverlayContainer
-        map={map}
-        position={{
-          lat: apartment.lat,
-          lng: apartment.lng
-        }}
-        key={index}
-      >
-        <MapPoint
-          image={apartment.image}
-          address={apartment.address}
-          area={apartment.area}
-          rooms_number={apartment.rooms_number}
-          floor={apartment.floor}
-          floor_count={apartment.floor_count}
-          rent={apartment.rent}
-        />
-      </OverlayContainer>
-    ))}
-  </div>;
+  return (
+    <>
+      <div ref={ref} id="map" className={classes.map}>
+        {apartments.map((apartment, index) => (
+          <OverlayContainer
+            map={map}
+            position={{
+              lat: apartment.lat,
+              lng: apartment.lng,
+            }}
+            key={index}
+          >
+            <MapPoint
+              image={apartment.image}
+              address={apartment.address}
+              area={apartment.area}
+              rooms_number={apartment.rooms_number}
+              floor={apartment.floor}
+              floor_count={apartment.floor_count}
+              rent={apartment.rent}
+            />
+          </OverlayContainer>
+        ))}
+      </div>
+      <button onClick={() => setApartments(Apartments)}>
+        Show apartments 1
+      </button>
+      <button onClick={() => setApartments(Apartments2)}>
+        Show apartments 2
+      </button>
+    </>
+  );
 }
 
-export default Map
+export default Map;
